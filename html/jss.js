4f9f7326d022d682682d7242e6ac0e40969a272d
nghiaconan123 <nghiaconan123@users.noreply.github.com>
2020-12-03 19:02:51 +0700

    Add files via upload

---
+++ html/jss.js
@@ -0,0 +1,105 @@
+	const sidebarContent = [
+			{
+				"thumbnailUrl": "CaoBaQuatBattle.gif",
+				"title": "Khởi nghĩa Cao Bá Quát",
+				"content": "Công cuộc còn đang trong giai đoạn chuẩn bị, thì bị người tố giác. Vua Tự Đức liền lệnh cho Tổng đốc Hà Ninh Lâm Duy Hiệp, Tổng đốc Sơn Hưng Tuyên Nguyễn Bá Nghi, hiệp cùng Tổng đốc Bắc Ninh là Nguyễn Quốc Hoan đi truy bắt Đảng nghịch. Để nhanh chóng đánh dẹp, nhà vua còn phái Vệ úy Hoàng thành Huế đem ngay một vệ lính tuyển phong, 15 võ sinh cùng 20 súng thần cơ ra ngay Hà Nội để hỗ trợ việc tiễu phạt. Trước cục diện này, Cao Bá Quát đang ở Bắc Ninh vội trở về Sơn Tây bàn ngày khởi sự. Do lực lượng ở các tỉnh chưa được chuẩn bị chu đáo, nên khi lệnh khởi nghĩa được phát ra thì chỉ có nghĩa quân ở Mỹ Lương (huyện Mỹ Lương tỉnh Sơn Tây, nay là phần đất phía Tây huyện Chương Mỹ Hà Nội và đất huyện Kim Bôi Hòa Bình) do Cao Bá Quát và Đinh Công Mỹ trực tiếp chỉ huy là kịp nổi lên.",
+				"thumbnailUrl1":"CaoBaQuat.png",
+				"URL":"CaoBaQuat.html",
+				"thumbnailUrl2":"NguyenBaNghi.png",
+				"URL1":"NguyenBaNghi.html",
+			},
+			{
+				"thumbnailUrl": "NongVanVanBattle.gif",
+				"title": "Khởi nghĩa Nông Văn Vân",
+				"content": "Khi Nông Văn Vân phất cờ chống nhà Nguyễn thì ở miền Bắc đang có cuộc nổi dậy của Lê Duy Lương và ở miền Nam đang có cuộc nổi dậy của Lê Văn Khôi.Trong cục diện ấy, Nông Văn Vân chia quân đi đánh phá các tỉnh miền ngược. Các quan sở tại chống không nổi, phải xin quân cứu viện. Tháng 8 (âm lịch) năm 1833, vua Minh Mạng cho Tổng đốc Sơn Hưng Tuyên là Lê Văn Đức làm Tổng đốc Tam Tuyên quân vụ, Thự tổng đốc Hải An là Nguyễn Công Trứ làm Tham tán, cùng đến mặt trận Tuyên Quang. Lại sai Tổng đốc Ninh Thái là Nguyễn Đình Phổ đem binh tượng tới mặt trận Thái Nguyên. Sau thấy hai thành tỉnh là Cao Bằng và Lạng Sơn bị vây ngặt quá, tháng 10 (âm lịch) nhà vua lại sai An Tĩnh tổng đốc là Tạ Quang Cự làm Tổng thống Đại thần lên đánh ở hai mạn đó.Tháng Chạp năm Quý Tỵ (1833) đạo quân của Tạ Quang Cự giải vây được tỉnh Lạng Sơn và lấy lại thành Cao Bằng. Đạo quân của Lê Văn Đức và Nguyễn Công Trứ vào đến Vân Trung ở Bảo Lạc là đại bản doanh của quân nổi dậy, khiến Nông Văn Vân phải chạy trốn sang Trung Quốc. Nhưng khi quân triều vừa rút đi, thì Nông Văn Vân lại về rủ đồ đảng đánh phá như cũ. Nhà vua lại sai các đạo quân tiến lên phòng giữ các tỉnh mạn ngược. Đến tháng 9 (âm lịch) năm Giáp Ngọ (1834), Lê Văn Đức và Phạm Văn Điển đi từ Sơn Tây lên Tuyên Quang; Tạ Quang Cự, Nguyễn Tiến Lâm và Hồ Hữu đi từ Cao Bằng; Nguyễn Đình Phổ và Nguyễn Công Trứ đi từ Thái Nguyên; ba mặt quân cùng tiến lên hội tiễu.Tháng Chạp năm ấy, cả ba đạo cùng đến hội ở Vân Trung, rồi sai người đưa thư sang nhờ quan nhà Thanh (Trung Quốc) ngăn không cho quân nổi dậy chạy sang bên ấy. Nên khi Nông Văn Vân chạy sang Trung Quốc thì bị quân nhà Thanh đuổi bắt, phải trở về Tuyên Quang.Tháng 3 (âm lịch) năm Ất Mùi (1835), biết chỗ Nông Văn Vân ở, quan quân nhà Nguyễn liền lần theo truy đuổi khiến ông Vân phải chạy ẩn vào trong rừng Thẩm Pát ở Thái Nguyên, rồi bị thiêu chết khi quân triều phóng hỏa đốt rừng. Cuộc nổi dậy do ông và các đồng đội đã dày công gầy dựng đến đây là kết thúc.",
+				"thumbnailUrl1":"NongVanVan.png",
+				"URL":"NongVanVan.html",
+				"thumbnailUrl2":"NguyenCongTru.png",
+				"URL1":"NguyenCongTru.html",
+				
+				
+			},
+			{
+				"thumbnailUrl": "PhanBaVanhBattle.gif",
+				"title": "Khởi nghĩa Phan Bá Vành",
+				"content": "Với chủ trương lấy của nhà giàu chia cho dân nghèo, ngay từ giai đoạn đầu, người đi theo đã có hơn 5.000, về sau thêm mấy ngàn quân của thủ lĩnh Ba Hùm (người Mường) từ thượng du Thanh Hóa cùng với quân nổi dậy ở các tỉnh lân cận kéo đến hiệp lực, thì lực lượng của ông đã lên đến hàng vạn. Tháng 2 (âm lịch) năm Minh Mạng thứ 7 (1826), từ đại bản doanh tại thôn Phú Nhai, thuộc làng Trà Lũ (nay thuộc 3 xã Xuân Trung, Xuân Bắc, Xuân Phương huyện Xuân Trường, tỉnh Nam Định), Phan Bá Vành dẫn quân đi đánh chiếm đồn Trà Lý và đồn Lân Hải (Kiến Xương, thuộc Thái Bình), giết được hai viên thủ ngự sứ là Đặng Đình Liễu và Nguyễn Trung Diễn. Tiếp đó, thủ lĩnh Ba Vành cho quân đánh lan ra vùng Kiến Xương, Vũ Tiên, Chân Ninh (thời Thành Thái đổi thành Trực Ninh)... Trấn thủ Sơn Nam Lê Mậu Cúc hay được, mang quân đến đàn áp. Hai bên giao chiến ác liệt tại Cồn Tiên (Tiền Hải). Đến khi trấn thủ Cúc tử trận, thì quân triều quăng vũ khí, bỏ thuyền bè mà chạy cả",
+				"thumbnailUrl1":"PhanBaVanh.png",
+				"URL":"PhanBaVanh.html",
+				"thumbnailUrl2":"NguyenCongTru.png",
+				"URL1":"NguyenCongTru.html",
+			},
+			{
+				"thumbnailUrl": "Kinh_thanh_Hue.jpg",
+				"title": "Kinh thành Huế",
+				"content": "Từ thời các chúa Nguyễn, Huế đã từng được chọn làm thủ phủ xứ Đàng Trong: năm 1635-1687 Nguyễn Phúc Lan, Nguyễn Phúc Tần dựng phủ ở Kim Long; đến thời Nguyễn Phúc Thái, Nguyễn Phúc Khoát đã dời phủ về Phú Xuân trong những năm 1687-1712; 1739-1774. Đến thời Tây Sơn, Huế vẫn được vua Quang Trung chọn làm thành kinh đô cho vương quốc của ông. Năm 1802, khi Nguyễn Phúc Ánh lên ngôi hoàng đế, mở đầu cho vương triều Nguyễn kéo dài suốt 143 năm, một lần nữa lại chọn Huế làm nơi đóng đô.",
+			    "thumbnailUrl1":"",
+				"URL":"",
+				"thumbnailUrl2":"",
+				"URL1":"",
+			},
+			{
+				"thumbnailUrl": "HaTienBattle.gif",
+				"title": "Khởi nghĩa Hà Tiên",
+				"content": "Năm Canh Tý (1840), hàng ngàn người dân bất mãn với chính sách cai trị của nhà Nguyễn đã tụ tập tại một số nơi trong tỉnh Hà Tiên (Việt Nam) làm thành nhiều cuộc nổi dậy lớn nhỏ, diễn ra dai dẳng, gây tổn thất nặng nề cả hai phía.Được tin quân nổi dậy đã có mặt nhiều nơi ở Nam Bộ, chiến thuyền quân Xiêm đang có mặt ở bờ biển Hà Tiên. Vua Thiệu Trị bèn sai Tả quân chưởng phủ Phạm Văn Điển mang một đạo binh lớn từ kinh đô Huế kéo vào gắp để trấn áp và chống ngăn. Là một vị tướng dày dạn kinh nghiệm, Phạm Văn Điển không cho quân đánh vào một điểm, mà chia ra đánh vào nhiều điểm, nhằm chia sức đối phó của đối phương. Cách thức này rất có hiệu quả, quân nổi dậy liên tiếp thua to.Theo sử liệu, thì mãi cho đến tháng 2 năm Đinh Mùi (1847), sau khi Nặc Ông Đôn làm vua Chân Lạp, quan quân Việt ở Trấn Tây thành (tức Phnom Penh ngày nay) rút hết về An Giang, thì chiến sự ở Nam Bộ (trong đó có tỉnh Hà Tiên) mới được yên.",
+				"thumbnailUrl1":"PhamVanDien.png",
+				"URL":"PhamVanDien.html",
+				"thumbnailUrl2":"",
+				"URL1":"",
+			},
+			{
+				"thumbnailUrl": "BaXuyenBattle.gif",
+				"title": "Khởi nghĩa Ba Xuyên",
+				"content": "Cuộc nổi dậy ở Ba Xuyên năm 1841, là một cuộc nổi dậy chống lại nhà Nguyễn thời vua Thiệu Trị, xảy ra trên địa bàn phủ Ba Xuyên lúc bấy giờ (nay là tỉnh Sóc Trăng, Việt Nam), do hai thủ lĩnh là Sơn Tốt và Trần Lâm cùng chỉ huy, khởi phát từ tháng 3 năm Tân Sửu (1841) đến khoảng đầu năm sau (1842) thì bị đánh tan. Khởi đầu, Sơn Tốt và Trần Lâm chia lực lượng ra làm hai, một cánh bao vây phủ thành Ba Xuyên.[6], một cánh kéo lên phía Bắc đánh phá huyện thành Vĩnh Định (ở xã Tân An, thành phố Cần Thơ hiện nay). Hốt hoảng, viên Tri huyện Vĩnh Định trốn chạy, đồng thời cấp báo lên cấp trên ở tỉnh Vĩnh Long. Tổng đốc Long Tường (Vĩnh Long & Định Tường) Dương Văn Phong thân đem quân đến cứu, chiếm lại được huyện lỵ Vĩnh Định, rồi tiến luôn đến Bãi Xàu, để giải vây cho phủ thành Ba Xuyên. Ở phía ngoài phủ thành Ba Xuyên, một trận đánh quyết liệt đã diễn ra, làm thủ lĩnh Sơn Tốt chết ngay tại trận.",
+				"thumbnailUrl1":"NguyenTienLam.png",
+				"URL":"NguyenTienLam.html",
+				"thumbnailUrl2":"",
+				"URL1":"",
+			},
+			{
+				"thumbnailUrl": "Bien.gif",
+				"title": "Cuộc bạo loạn ven biển",
+				"content": "Cuộc nổi dậy Tạ Văn Phụng, hay còn gọi là Cuộc bạo loạn ven biển hoặc Nạn giặc biển; là tên gọi của cuộc nổi dậy do Tạ Văn Phụng lãnh đạo chống lại triều đình nhà Nguyễn ở vùng ven biển Bắc Kỳ từ 1861 cho tới 1865. Để tăng thêm thanh thế, Phụng liên kết với Nguyễn Văn Thịnh, tức Cai tổng Vàng, đánh phủ Lạng Giang, huyện Yên Dũng và Bắc Ninh nhiều phen rất nguy ngập. Đă có lần Lê Duy Phụng cho người vào Nam Kỳ điều đình với Thiếu tướng Bonnard đem quân ra giúp, hứa nếu thành công thì ông sẽ để cho Pháp bảo hộ nhưng không thành vì lúc này Pháp còn lo củng cố xứ Nam Kỳ và đang cần tạm thời có sự hòa hảo với triều đình Huế. Sau quân của Phụng chiếm miền duyên hải để tiện việc tiến thoái. Tình hình nguy ngập nên tháng 6 năm 1863, triều đình lại phải phái Nguyễn Tri Phương thay Trương Quốc Dụng. Cuối năm 1863, Lê Duy Phụng tập hợp hơn 500 chiến thuyền ở Cát Bà và núi Đồ Sơn để đem quân đi đánh kinh đô Huế nhưng gặp bão bị đắm rất nhiều. Tháng 5 năm 1865 quân của Phụng với hơn 300 chiến thuyền lại từ hải đảo vào cướp phá miền duyên hải nhưng bị Nguyễn Tri Phương sai Nguyễn Văn Vỹ hợp cùng hải quân nhà Thanh đón đánh, quân của Phụng thảm bại. Từ đó về sau quân khởi nghĩa liên tiếp bại trận. Tháng 8 năm 1865, Đốc binh Ông Ích Khiêm cùng với quân Thanh ở Khâm Châu tiến công, lấy lại thành Hải Ninh bị chiếm đã lâu, bộ hạ của Phụng bị bắt rất nhiều. Lê Duy Phụng cũng phải bỏ chạy vào Trung Kỳ nhưng bị bắt và bị giải về kinh đô Huế xử tử.",
+				"thumbnailUrl1":"TavanPhung.png",
+				"URL":"TaVanPhung.html",
+				"thumbnailUrl2":"NguyenTriPhuong.png",
+				"URL1":"NguyenTriPhuong.html",
+			},
+			
+			
+		];
+
+		var currentItemId = -1;
+
+		$(".mapItem").click(function(event) {
+			event.preventDefault();
+
+			const id = event.target.dataset.id;
+
+			if (currentItemId !== id) {
+				currentItemId = id;
+				if (currentItemId >= 0 && currentItemId < sidebarContent.length) {
+					$('#itemContent #title').text('');
+					$('#itemContent #content').text('');
+					$('#itemContent #image').attr("src", '');
+					$('#loading').html('<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>');
+						
+					setTimeout(function(){
+						$('#itemContent #title').text(sidebarContent[currentItemId].title);
+						$('#itemContent #content').text(sidebarContent[currentItemId].content);
+						$('#itemContent #image').attr("src", sidebarContent[currentItemId].thumbnailUrl);
+						$('#itemContent #image1').attr("src", sidebarContent[currentItemId].thumbnailUrl1);
+						$('#itemContent #image2').attr("src", sidebarContent[currentItemId].thumbnailUrl2);
+						$('#itemContent #url').attr("href", sidebarContent[currentItemId].URL);
+						$('#itemContent #url1').attr("href", sidebarContent[currentItemId].URL1);
+						$('#loading').empty();
+					}, 300);
+				}
+			}
+		  $("#mySidebar").css({left: "0px"});
+		  $("#main").css({marginLeft: "400px"});
+		});
+
+		function closeNav() {
+		  $("#mySidebar").css({left: "-400px"});
+		  $("#main").css({marginLeft: "0px"});
+		}
---
+++ html/script.js
@@ -0,0 +1,6 @@
+//https://ihatetomatoes.net/create-custom-preloading-screen/
+$(document).ready(function() {
+  setTimeout(function() {
+    $('.load-wrapper').fadeOut();
+  },3000);
+});
